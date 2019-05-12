9e421c918c8b80c0fcf892f1fc5703bd0413314e
Kim.McK <k.mckeever@alumni.unc.edu>
2019-05-12 15:44:34 -0400

    minor app edits

--- static/js/alien_app.js
+++ static/js/alien_app.js
@@ -3,32 +3,32 @@
 var tableData = ufo_data;
 
 // select tbody using ds
-tbody = d3.select("tbody")
+var tbody = d3.select("tbody")
 
 // using Object.entries to get into key and value pairs of data inside of the table
 // and loop through them to add to the table in html
 function displayData(data){ 
     tbody.text("")
-    data.forEach(function(sighting){
+    data.forEach((sighting)=> {
     new_tr = tbody.append("tr")
-    Object.entries(sighting).forEach(function([key, value]){
+    Object.entries(sighting).forEach(([key, value])=> {
         new_td = new_tr.append("td").text(value)	
     })
 })}
 
 displayData(tableData)
 
-var inputText = d3.select("#datetime")
+var inputDate = d3.select("#datetime")
 var button = d3.select("filter-btn")
 
 // filter data with desired date
 function changeHandler(){
     d3.event.preventDefault();
-    console.log(inputText.property("value"));
-    var new_table = tableData.filter(sighting => sighting.datetime===inputText.property("value"))
+    console.log(inputDate.property("value"));
+    var new_table = tableData.filter(sighting => sighting.datetime===inputDate.property("value"))
     displayData(new_table)
 }
 
 // event listener to handle change and click
-inputText.on("change", changeHandler)
+inputDate.on("change", changeHandler)
 button.on("click", changeHandler)
